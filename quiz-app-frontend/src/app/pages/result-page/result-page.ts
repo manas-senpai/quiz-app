c11459908604e88b54a141f66eba9d876e32b23b
Atharavraj Dhumal <manasdhumal0@gmail.com>
2026-03-26 11:39:17 +0530

    bare minimum result page done

--- quiz-app-frontend/src/app/pages/result-page/result-page.ts
+++ quiz-app-frontend/src/app/pages/result-page/result-page.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar'; 
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-result-page',
-  imports: [],
+  imports: [MatProgressBarModule],
   templateUrl: './result-page.html',
   styles: ``,
 })
 export class ResultPage {
+
+  constructor(private router: Router) { }
+
+  retry() {
+    this.router.navigate(['/quiz']);
+  }
+
+  home() {
+    this.router.navigate(['/']);
+  }
 
 }
